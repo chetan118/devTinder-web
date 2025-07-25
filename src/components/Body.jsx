ea226d3569df61a86886635804d2bd56f8876c29
chetan118 <chetan.sakpal@gmail.com>
2025-07-25 23:23:07 +0530

    Logged in user should not access the login page
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -14,12 +14,18 @@ const Body = () => {
   const user = useSelector((state) => state.user);
 
   const fetchUser = async () => {
-    if (user) return;
     try {
-      const res = await axios.get(BASE_URL + "/profile/view", {
-        withCredentials: true,
-      });
-      dispatch(addUser(res.data));
+      if (!user) {
+        const res = await axios.get(BASE_URL + "/profile/view", {
+          withCredentials: true,
+        });
+        dispatch(addUser(res.data));
+      } else {
+        if (location.pathname === "/login") {
+          // user is logged in and tries to visit the login page
+          navigate("/");
+        }
+      }
     } catch (err) {
       if (err.status === 401) {
         navigate("/login");
