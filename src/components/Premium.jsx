874d400ed5f18de4419eb82337b37aeb855efde4
chetan118 <chetan.sakpal@gmail.com>
2025-09-09 11:20:32 +0530

    Modify the premium page to detect premium user
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,7 +1,19 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/user/ispremium", {
+      withCredentials: true,
+    });
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
   const handleBuyClick = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
@@ -28,12 +40,20 @@ const Premium = () => {
       theme: {
         color: "#F37254",
       },
+      handler: verifyPremiumUser,
     };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
-  return (
+
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
+  return isUserPremium ? (
+    "You are a premium user."
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
