bc71f069e3a957a19760affabdd8b3b394027376
chetan118 <chetan.sakpal@gmail.com>
2025-11-25 09:30:00 +0530

    feat: show loading spinner during initial auth check in Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,17 +5,21 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { addUser } from "../utils/userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   const user = useSelector((state) => state.user);
+  const [loading, setLoading] = useState(true);
 
   const fetchUser = async () => {
     try {
-      if (user) return;
+      if (user) {
+        setLoading(false);
+        return;
+      }
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
@@ -25,6 +29,8 @@ const Body = () => {
         navigate("/login");
       }
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -37,6 +43,14 @@ const Body = () => {
       navigate("/");
     }
   }, [user, location.pathname, navigate]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center mt-20">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
 
   return (
     <div>
