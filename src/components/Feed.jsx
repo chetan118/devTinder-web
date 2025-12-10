8cf0c9c76cefbb3b47aa6fbaabe7ddab24262410
chetan118 <chetan.sakpal@gmail.com>
2025-12-10 15:30:00 +0530

    feat: show loading spinner while feed is loading

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,27 +3,39 @@ import UserCard from "./UserCard";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Feed = () => {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
   const feed = useSelector((state) => state.feed);
 
   const getFeed = async () => {
     if (feed) return;
     try {
+      setIsLoading(true);
       const res = await axios.get(BASE_URL + "/user/feed", {
         withCredentials: true,
       });
       dispatch(addFeed(res?.data?.users));
     } catch (err) {
       console.error(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     getFeed();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center mt-20">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
 
   if (!feed || feed.length <= 0) {
     return (
